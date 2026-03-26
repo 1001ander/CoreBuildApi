cb3638d877a42acf8f1e9b33e71e481212db8d88
Anderson Nunez Consoro <an8634335@gmail.com>
2026-03-26 18:44:49 -0400

    Crear la carpeta data y el contexto.

---
+++ CoreBuildApi/data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CoreBuildApi.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreBuildApi.data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Cpu> Cpus { get; set; }
+    public DbSet<Gpu> Gpus { get; set; }
+    public DbSet<Motherboard> Motherboards { get; set; }
+    public DbSet<Ram> Rams { get; set; }
+    public DbSet<Psu> Psus { get; set; }
+}

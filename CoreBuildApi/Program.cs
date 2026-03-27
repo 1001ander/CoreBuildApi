de15a57928aaf3a1f7299f129629007434e08099
Anderson Nunez Consoro <an8634335@gmail.com>
2026-03-27 00:06:20 -0400

    agregar seed de datos para CPU, GPU, Motherboard, RAM y PSU.

--- CoreBuildApi/Program.cs
+++ CoreBuildApi/Program.cs
@@ -12,9 +12,16 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DataSeeder.SeedAsync(context);
+}
+
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
